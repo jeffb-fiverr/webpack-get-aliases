e667e250da3de9549b58d87115c6693157551fb9
Jeff Borders <jeff.b@fiverr.com>
2016-11-22 18:56:18 -0600

    initial

---
+++ index.js
@@ -0,0 +1,30 @@
+var _ = require('lodash');
+
+function getDepListAliases(depList) {
+
+    var aliasStore = {},
+        currPkg;
+
+    _.each(depList, function(pkgVersion, pkgName) {
+
+        if (!_.has(depList, pkgName)) return;
+
+        currPkg = require('./node_modules/' + pkgName + '/package.json');
+
+        if (!currPkg.moduleAliases) return;
+
+        console.log("Adding aliases from: " + currPkg.name);
+
+        aliasStore = _.extend(aliasStore, currPkg.moduleAliases);
+    });
+
+    return aliasStore;
+};
+
+module.exports = function(pkg) {
+
+    var depsAliases = iterator(pkg.dependencies),
+        devDepsAliases = iterator(pkg.devDependencies);
+
+    return _.extend(devDepsAliases, depsAliases);
+};
